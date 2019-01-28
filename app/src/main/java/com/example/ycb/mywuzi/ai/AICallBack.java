22a522fe3a6b877f4cfc5a8b426c65d33899da76
yang <yangchangbiao@ysbang.cn>
2019-01-28 09:42:14 +0800

    上一版本漏提交

---
+++ app/src/main/java/com/example/ycb/mywuzi/ai/AICallBack.java
@@ -0,0 +1,10 @@
+package com.example.ycb.mywuzi.ai;
+
+/**
+ * Created by ZhangHao on 2017/7/25.
+ * AI落子完成回调
+ */
+
+public interface AICallBack {
+    void aiAtTheBell(int x,int y);
+}
---
+++ app/src/main/java/com/example/ycb/mywuzi/util/Const.kt
@@ -0,0 +1,32 @@
+package com.example.ycb.mywuzi.util
+
+/**
+ * Created by biao on 2019/1/25.
+ */
+class Const{
+    companion object {
+        /**
+         * 一些常量
+         */
+        //白棋
+        val WHITE_CHESS = 1
+        //黑棋
+        val BLACK_CHESS = 2
+        //无棋
+        val NO_CHESS = 0
+        //白棋赢
+        val WHITE_WIN = 101
+        //黑棋赢
+        val BLACK_WIN = 102
+        //平局
+        val NO_WIN = 103
+
+        val MODEL_TYPE = "MODEL_TYPE"
+        //人人模式
+        val MODEL_TYPE_RENREN = 1
+        //人机模式
+        val MODEL_TYPE_RENJI = 2
+        //蓝牙模式
+        val MODEL_TYPE_BLUE = 3
+    }
+}
